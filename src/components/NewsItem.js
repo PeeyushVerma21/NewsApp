ddb4230fa4aad6ef570c682e5799cebefc1fa28e
PeeyushVerma21 <peeyushverma00@gmail.com>
2024-08-30 23:39:22 +0530

    first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span class="badge rounded-pill bg-danger">{source}</span>
+        </div>
+        <img
+          src={
+            imageUrl
+              ? imageUrl
+              : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuINn-ULoWD6MXpCn71cAtFXVgRgfeKFWY2Q&s"
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              By {author ? author : "Unknown"} on {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blank"
+            className="btn btn-sm btn-dark"
+          >
+            Read More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
